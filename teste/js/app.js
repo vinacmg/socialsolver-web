089b04c3b59144d84a23cc92246430f0f1c7fd43
Vinícius <viniciuscmgo@gmail.com>
2018-05-18 20:18:45 -0300

    initial commit

---
+++ teste/js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCUr8wEpToFZ3UMnw3Kj8OAYYpIzRicIhk",
+  authDomain: "socialsolver-app.firebaseapp.com",
+  databaseURL: "https://socialsolver-app.firebaseio.com",
+  projectId: "socialsolver-app",
+  storageBucket: "socialsolver-app.appspot.com",
+  messagingSenderId: "329381999567"
+};
+firebase.initializeApp(config);
